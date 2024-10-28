a3fd34d8bf5fc5c540d0b53df3738ac923fccfb9
Manish kumar <manishkumar2282000@gmail.com>
2024-10-28 22:48:18 +0530

    frontend part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-
+import { PipelineToolbar } from './components/toolbar';
+import { PipelineUI } from './components/pipelineUI';
+import { SubmitButton } from './components/submit';
+import { ReactFlowProvider } from 'reactflow';
 
 function App() {
-  
-
   return (
     
       <div>
-        <h1> hello world</h1> 
-      </div>
-      
-  )
+        <ReactFlowProvider>
+          <PipelineToolbar />
+          <PipelineUI />
+          <SubmitButton />
+        </ReactFlowProvider> 
+      </div> 
+  );
 }
 
 export default App
